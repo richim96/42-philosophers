b9aac1702e0f855c87a8fbdfc5217a4020336ce2
Riccardo Mei <riccardo.mei@andercore.com>
2025-05-22 23:29:40 +0200

    fix(philo/src): move functions due to cursor fuckup

--- philo/src/utils.c
+++ philo/src/utils.c
@@ -6,7 +6,7 @@
 /*   By: rmei <rmei@student.42berlin.de>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 19:45:33 by rmei              #+#    #+#             */
-/*   Updated: 2025/05/22 23:24:03 by rmei             ###   ########.fr       */
+/*   Updated: 2025/05/22 23:28:17 by rmei             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -92,7 +92,7 @@ void ft_usleep(long long time)
  * @param philo Pointer to the philosopher structure
  * @param status Status message to print
  */
-void ft_print(t_philo *philo, char *status)
+void ft_print_status(t_philo *philo, char *status)
 {
     pthread_mutex_lock(&philo->data->print);
     if (!philo->data->is_dead)
@@ -103,40 +103,4 @@ void ft_print(t_philo *philo, char *status)
             status
         );
     pthread_mutex_unlock(&philo->data->print);
-}
-
-/**
- * @brief Checks if a philosopher has died
- * @param philo Pointer to the philosopher structure
- * @return 1 if philosopher has died, 0 otherwise
- */
-int ft_check_death(t_philo *philo)
-{
-    pthread_mutex_lock(&philo->data->death);
-    if (philo->data->is_dead)
-    {
-        pthread_mutex_unlock(&philo->data->death);
-        return (1);
-    }
-    pthread_mutex_unlock(&philo->data->death);
-    return (0);
-}
-
-/**
- * @brief Checks if all philosophers have eaten the required number of times
- * @param data Pointer to the data structure
- * @return 1 if all philosophers have eaten enough, 0 otherwise
- */
-int ft_check_meals(t_data *data)
-{
-    int i;
-
-    i = 0;
-    while (i < data->num_philos)
-    {
-        if (data->philos[i].times_eaten < data->must_eat)
-            return (0);
-        i++;
-    }
-    return (1);
 }
